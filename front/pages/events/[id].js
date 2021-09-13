a78d46bc4e7fc2d6d24a3052c4a0994a150eb1e7
nas-be <naserbechata@gmail.com>
2021-09-13 17:09:49 +0200

    initial commit

---
+++ front/pages/events/[id].js
@@ -0,0 +1,7 @@
+export default function edit() {
+    return (
+        <div>
+            Edit event {id}
+        </div>
+    )
+}
---
+++ front/pages/events/index.js
@@ -0,0 +1,7 @@
+export default function EventsPage() {
+    return (
+        <div>
+            <h1>Events page</h1>
+        </div>
+    )
+}
